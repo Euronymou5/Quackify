76b36fb07b8fbfc523bc3467d8a97c3b68160b67
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-12-31 01:22:44 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+name = "quackify"
+version = "0.1"
+description = "Quackify es una biblioteca para Python que emula diversas funciones del duckyscript."
+author = "Euronymou5"
+url = "https://github.com/Euronymou5/Quackify"
+license = "Mozilla Public License 2.0"
+install_requires = ["pyautogui", "pynput"]
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description=description,
+    url=url,
+    license=license,
+    packages=["quackify"],
+    author=author,
+    classifiers=[
+       "License :: OSI Approved :: Mozilla Public License 2.0 (MPL-2.0)",
+       "Programming Language :: Python :: 3",
+       "Topic :: Software Development :: Libraries :: Python Modules",
+       "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+)
